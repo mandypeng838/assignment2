efe217f7950d5babc77e7473bd6ed4096ac18558
Windows <Windows@DESKTOP-CST0DDH.lan>
2025-06-01 19:53:48 -0400

    Added quiz.java JFrame

--- src/quiz.java
+++ src/quiz.java
@@ -5,14 +5,14 @@
 
 /**
  *
- * @author mandypeng
+ * @author Windows
  */
-public class assignment2 extends javax.swing.JFrame {
+public class quiz extends javax.swing.JFrame {
 
     /**
-     * Creates new form assignment2
+     * Creates new form quiz
      */
-    public assignment2() {
+    public quiz() {
         initComponents();
     }
 
@@ -58,20 +58,20 @@ public class assignment2 extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(assignment2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(quiz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(assignment2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(quiz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(assignment2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(quiz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(assignment2.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(quiz.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new assignment2().setVisible(true);
+                new quiz().setVisible(true);
             }
         });
     }
